e8bcecd3cf5dbb9b4099f86f5a60044886154105
wangzb96 <wangzb96@foxmail.com>
2020-08-11 23:52:34 +0800

    测试红黑树

--- src/pers/wangzb96/test/TestTree.java
+++ src/pers/wangzb96/test/TestTree.java
@@ -1,6 +1,8 @@
 package pers.wangzb96.test;
 
 import java.util.Random;
+import java.util.TreeMap;
+
 import pers.wangzb96.dsalg.tree.*;
 import static pers.wangzb96.util.Util.*;
 
@@ -59,30 +61,62 @@ public interface TestTree{
         public TestRedBlackTree(){
             super(Integer::compare);
         }
-        public void test(int max, int num, int batch){
+        public long test(int max, int num, double prob, double prob1,
+                         boolean val, int batch, boolean p){
             final double PROB = 1.0/3;
             final double PROB1 = 2.0/3;
             final long SEED = 666;
             var rd = new Random(SEED);
+            var start = getTimeMS();
             for(var i=0; i<num; ++i){
                 var elem = rd.nextInt(max);
-                var prob = rd.nextDouble();
-                if(prob<PROB) insert(elem);
-                else if(prob<PROB1) remove(elem);
+                var prb = rd.nextDouble();
+                if(prb<prob) insert(elem);
+                else if(prb<prob1) remove(elem);
                 else get(elem);
-                if((i<10 || i%batch==0) && !validate()){
-                    error("红黑树不合法！");
+                if(val && (i<10 || i%batch==0) && !validate()){
+//                    error("红黑树不合法！");
+                    error("*** RBTree ERROR! ***");
                     exit();
                 }
             }
-            println(this);
-            printLine();
+            var elap = getTimeMS()-start;
+            if(p) println(this);
+            return elap;
+        }
+    }
+    class TestTreeMap{
+        @SuppressWarnings({"MismatchedQueryAndUpdateOfCollection", "UnnecessaryLocalVariable"})
+        public long test(int max, int num, double prob, double prob1){
+            var map = new TreeMap<Integer, Object>();
+            final long SEED = 666;
+            var rd = new Random(SEED);
+            var start = getTimeMS();
+            for(var i=0; i<num; ++i){
+                var elem = rd.nextInt(max);
+                var prb = rd.nextDouble();
+                if(prb<prob) map.put(elem, elem);
+                else if(prb<prob1) map.remove(elem);
+                else map.get(elem);
+            }
+            var elap = getTimeMS()-start;
+            return elap;
         }
     }
 
     static void main(String[] args){
-        new TestBinaryTree().test();
-        new TestBinarySearchTree().test();
-        new TestRedBlackTree().test(10000, 100_0000, 100);
+//        new TestBinaryTree().test();
+//        new TestBinarySearchTree().test();
+        var max = 1_00_0000;
+        var num = 1_00_0000;
+        var prob = 0.5;
+        var prob1 = 0.5;
+        for(var i=0; i<10; ++i){
+            var elap = new TestRedBlackTree().test(max, num, prob, prob1,
+                                                   false, 100, false);
+            var elap1 = new TestTreeMap().test(max, num, prob, prob1);
+            println("%d | %.3f | %.3f (S)".formatted(i, elap/1000.0, elap1/1000.0));
+        }
+        printLine();
     }
 }
